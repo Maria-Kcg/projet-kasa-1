3ae36dc20068d75f420246bd936c95c097710c41
Maria-Kcg <kpechegomariegrace@gmail.com>
2026-03-29 02:28:33 +0100

    création du routeur, des pages et composants

---
+++ src/Routeur.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error from "./pages/Error";
+
+function Routeur() {
+    return (
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<About />} />
+                <Route path="*" element={<Error />} />
+            </Route>
+        </Routes>
+    )
+}
+
+export default Routeur;
